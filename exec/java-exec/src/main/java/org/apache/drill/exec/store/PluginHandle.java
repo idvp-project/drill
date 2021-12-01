d4f04f993ded8817ebed0ea813570c3ff00d0846
andrew <andrew@home>
2021-12-01 13:53:44 +0300

    use shaded org.apache.drill.shaded.guava.com.google.common

--- exec/java-exec/src/main/java/org/apache/drill/exec/store/PluginHandle.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/store/PluginHandle.java
@@ -17,7 +17,7 @@
  */
 package org.apache.drill.exec.store;
 
-import com.google.common.base.Stopwatch;
+import org.apache.drill.shaded.guava.com.google.common.base.Stopwatch;
 import org.apache.drill.common.exceptions.UserException;
 import org.apache.drill.common.logical.StoragePluginConfig;
 import org.apache.drill.shaded.guava.com.google.common.annotations.VisibleForTesting;
